021a2da7b238c80d13deacc03cddf4bbba9721a3
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-09 09:53:38 +0700

    display list product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,25 @@ class Product extends Model
         'short_description',
         'category_id'
     ];
+
+    /*    public function brand()
+        {
+            return $this->belongsTo('App\Models\Brand');
+        }*/
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function image()
+    {
+        return $this->hasOne(Image::class);
+    }
 }
