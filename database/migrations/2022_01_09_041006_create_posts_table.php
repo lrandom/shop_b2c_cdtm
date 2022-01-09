9cfa7bd24c76ef098ca182c71fbb90c3f4e01671
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-09 11:15:22 +0700

    migrate file for posts_table

---
+++ database/migrations/2022_01_09_041006_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->text('short_description');
+            $table->text('meta_keyword');
+            $table->text('meta_description');
+            $table->string('thumbnail_path');
+            $table->bigInteger('category_id');
+            $table->tinyInteger('type')->default(1);//1- normal, 2 special news
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- resources/views/be/commons/aside.blade.php
+++ resources/views/be/commons/aside.blade.php
@@ -85,29 +85,7 @@
                     </ul>
                 </li>
 
-                <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
-                        <i class="nav-icon fas fa-list"></i>
-                        <p>
-                            Product
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('admin.product.list')}}" class="nav-link active">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>List Product</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{route('admin.product.add')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Add Product</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+
 
 
                 {{--                  <li class="nav-item">
