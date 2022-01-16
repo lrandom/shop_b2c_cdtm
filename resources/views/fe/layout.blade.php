b513770526e6b6ceca29c29415c97596871d8cda
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-16 10:48:24 +0700

    layout for product section

--- resources/views/fe/layout.blade.php
+++ resources/views/fe/layout.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 <body>
-<div class="container mx-auto">
+<div class="container mx-auto space-y-10">
     <img src="{{asset('fe/img/logo.png')}}" width="160" class="mx-auto"/>
 
     <nav class="flex justify-between items-center shadow p-3" style="background: #f6f6f6">
@@ -24,17 +24,102 @@
         </div>
 
         <div class="flex items-center gap-2">
-            <input type="search" class="shadow border border-1 border-gray-200 p-2 w-64"
+            <input type="search" class="shadow border border-1 border-gray-400 p-2 w-64"
                    placeholder="search..."/>
             <div class="text-xl">
                 <a href=""><i class="bi bi-heart"></i></a>
                 <a href=""><i class="bi bi-person"></i></a>
                 <a href=""><i class="bi bi-cart"></i></a>
             </div>
+        </div>
+    </nav>
 
+    <!--BANNER-->
+    <div>
+        <img src="{{asset('fe/img/banner.jpg')}}" class="mx-auto object-cover h-96 w-full"/>
+    </div>
+    <!--END BANNER-->
+
+    <!--MAIN CONTENT-->
+
+    <div class="space-y-10">
+        <!--TOP PRODUCT-->
+        <div class="space-y-5">
+            <h3 class="font-bold text-xl">FEATURE PRODUCT <a style="color:#F2994A;" class="text-sm font-normal">See All</a>
+            </h3>
+            <div class="grid md:grid-cols-4 grid-cols-1 gap-10">
+                @for($i=0;$i<8;$i++)
+                    <div class="shadow">
+                        <div class="relative">
+                            <img src="{{asset('fe/img/shoes.jpeg')}}" class="object-cover h-64 w-full"/>
+                            <a class="absolute top-4 w-10 h-10 text-center rounded shadow right-4 p-2 bg-white">
+                                <i class="bi bi-heart"></i>
+                            </a>
+                        </div>
+
+
+                        <div class="px-4 py-2">
+                            <div class="flex justify-between items-center">
+                                <h3 class="font-bold text-lg">Product Name</h3>
+                                <a class="shadow py-1 px-2 text-center w-10 rounded" style="background: #F2C94C;">
+                                    <i class="bi bi-cart "></i>
+                                </a>
+                            </div>
+                            <div>
+                                <span class="text-gray-400 line-through">300$</span>
+                                <span class="text-red-500">300$</span>
+                            </div>
+                            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus,
+                                adipisci.</p>
+                        </div>
+                    </div>
+                @endfor
+            </div>
         </div>
 
-    </nav>
+        <!--NEW ARIVAL PRODUCT-->
+        <div class="space-y-5">
+            <h3 class="font-bold text-xl">NEW ARIVAL PRODUCT <a style="color:#F2994A;" class="text-sm font-normal">See
+                    All</a>
+            </h3>
+            <div class="grid md:grid-cols-4 grid-cols-1 gap-10">
+                @for($i=0;$i<8;$i++)
+                    <div class="shadow">
+                        <div class="relative">
+                            <img src="{{asset('fe/img/shoes.jpeg')}}" class="object-cover h-64 w-full"/>
+                            <a class="absolute top-4 w-10 h-10 text-center rounded shadow right-4 p-2 bg-white">
+                                <i class="bi bi-heart"></i>
+                            </a>
+                        </div>
+
+
+                        <div class="px-4 py-2">
+                            <div class="flex justify-between items-center">
+                                <h3 class="font-bold text-lg">Product Name</h3>
+                                <a class="shadow py-1 px-2 text-center w-10 rounded" style="background: #F2C94C;">
+                                    <i class="bi bi-cart "></i>
+                                </a>
+                            </div>
+                            <div>
+                                <span class="text-gray-400 line-through">300$</span>
+                                <span class="text-red-500">300$</span>
+                            </div>
+                            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus,
+                                adipisci.</p>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--END MAIN CONTENT-->
+
+
 </div>
 </body>
 </html>
