5b4eec28d34431d3617d478b35655cb110f4d9e9
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-21 09:30:11 +0700

    UI for footer

--- resources/views/fe/layout.blade.php
+++ resources/views/fe/layout.blade.php
@@ -154,15 +154,15 @@ C/O https://placeholder.com/"
     <!--END MAIN CONTENT-->
 
     <!--NEWSETTLER FORM-->
-    <div class="md:flex justify-between items-center p-10" style="background: #DFDFDF">
-        <div>
+    <div class="md:grid grid-cols-7 place-items-center p-10" style="background: #DFDFDF">
+        <div class="col-span-3">
             <h4 class="font-bold text-3xl">Đăng ký nhận bản tin</h4>
             <p class="font-bold text-base">Đừng bỏ lỡ hàng ngàn sản phẩm và các chuwong trình siêu hấp dẫn</p>
         </div>
 
-        <div>
-            <form class="flex gap-2 items-center border-2">
-                <input type="email" class="bg-white h-16 w-36 p-2"/>
+        <div class="col-span-4">
+            <form class="flex gap-2 items-center border-2 w-full">
+                <input type="email" class="bg-white h-16 w-48 p-2"/>
                 <button class="uppercase text-2xl h-16 font-bold bg-yellow-300 p-2">Get News</button>
             </form>
         </div>
@@ -171,8 +171,60 @@ C/O https://placeholder.com/"
     <!--END FORM-->
 
     <!--FOOTER-->
-    <footer>
+    <footer class="p-10 text-white" style="background: #4F4F4F">
+        <div class="grid md:grid-cols-4 grid-cols-1 md:gap-10">
+            <div class="space-y-5">
+                <img src="{{asset('fe/img/logo.png')}}" width="160" class="mx-left"/>
+                <hr class="border-1 border-white">
+                <ul class="space-y-5">
+                    <li>Address: Quang Ninh - VietNam</li>
+                    <li>Phone: 023928379</li>
+                    <li>Fax: 0181092812</li>
+                </ul>
+            </div>
 
+            <div class="space-y-5">
+                <h3 class="uppercase font-bold">About Us</h3>
+                <hr class="border-1 border-white">
+                <ul class="space-y-5">
+                    <li><a>Introduction</a></li>
+                    <li><a>Our franchise</a></li>
+                    <li><a>Career</a></li>
+                </ul>
+            </div>
+
+            <div class="space-y-5">
+                <h3 class="uppercase font-bold">Support</h3>
+                <hr class="border-1 border-white">
+                <ul class="space-y-5">
+                    <li>Order Guide</li>
+                    <li>Refund Policy</li>
+                    <li>Hotline</li>
+                </ul>
+            </div>
+
+            <div class="space-y-5">
+                <h3 class="uppercase font-bold">Social</h3>
+                <hr class="border-1 border-white">
+                <div class="flex gap-2">
+                    <span class="border-2 border-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <a href="">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                    </span>
+                    <span class="border-2 border-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <a href="">
+                           <i class="bi bi-instagram"></i>
+                        </a>
+                    </span>
+                    <span class="border-2 border-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <a href="">
+                         <i class="bi bi-youtube"></i>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
     </footer>
     <!--END FOOTER-->
 </div>
